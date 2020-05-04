a162cf12b927b202de64214fd1c6fd1e69fca633
antocecere <antocecere77@gmail.com>
2020-05-04 18:08:59 +0200

    More flexbox styling

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -25,16 +25,17 @@ const GoalInput = (props) => {
 };
 
 const styles = StyleSheet.create({
-    inputContainer: {
-      flexDirection: 'row', 
-      justifyContent: 'space-between', 
+    inputContainer: {      
+      flex: 1,
+      justifyContent: 'center', 
       alignItems: 'center'
     },
     input: {
       width: '80%', 
       borderColor: 'black', 
       borderWidth: 1, 
-      padding: 10
+      padding: 10,
+      marginBottom: 10
     }
   });
 
